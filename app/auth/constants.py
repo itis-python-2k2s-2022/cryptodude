35a592d5628ebfb7fd5d22d68ac21f4489e35bae
Orkhan Kazymov <rayskarken@gmail.com>
2022-04-25 15:19:46 +0300

    reformatted by black

--- app/auth/constants.py
+++ app/auth/constants.py
@@ -3,6 +3,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-SECRET_KEY = env.str('SECRET_KEY')
-ALGORITHM = env.str('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = env.int('ACCESS_TOKEN_EXPIRE_MINUTES')
+SECRET_KEY = env.str("SECRET_KEY")
+ALGORITHM = env.str("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = env.int("ACCESS_TOKEN_EXPIRE_MINUTES")
