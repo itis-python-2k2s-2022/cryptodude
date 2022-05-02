7b75d61771d45647adc06810e4b0ccf13df7884d
Bogdan Abarikhin <bogdan_abarihin@mail.ru>
2022-05-02 10:19:59 +0400

    + small updates

--- app/auth/endpoints.py
+++ app/auth/endpoints.py
@@ -64,7 +64,8 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return create_user(db, user)
+    create_user(db, user)
+    return {"status": status.HTTP_201_CREATED}
 
 
 @auth_app.get("/users/me", response_model=User)
